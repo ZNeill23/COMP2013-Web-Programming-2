413f32ced60b42a062df0400685d41dffe4b1294
ZNeill23 <zack.neill2012@gmail.com>
2025-09-21 14:07:23 -0400

    Small updates to diamond world

--- Tutorial-2/my-first-react-project/src/App.jsx
+++ Tutorial-2/my-first-react-project/src/App.jsx
@@ -7,11 +7,17 @@ function App() {
       <h1>Diamond World</h1>
       <div className="main-container">
         <Card image="src/assets/images/1.jpg" name="princess" price="$1,350" />
-        <Card image="src/assets/images/2.jpg" name="swan" price="$900" />
+        <Card
+          image="src/assets/images/2.jpg"
+          name="swan"
+          price="$900"
+          sale={true}
+        />
         <Card
           image="src/assets/images/3.jpg"
           name="collection"
           price="$1,100"
+          sale={true}
         />
       </div>
     </>
