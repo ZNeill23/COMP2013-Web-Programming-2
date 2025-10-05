14a478fa661870679f54a558f4678bfa80d30119
Zack Neill <149334445+ZNeill23@users.noreply.github.com>
2025-10-04 23:15:41 -0400

    Reformat and removed some comments

--- Lab-3/src/Components/ColorBox.jsx
+++ Lab-3/src/Components/ColorBox.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export default function ColorBox({ colors }) {
-  // When the page loads or the page is refreshed, the boxes will be a random color
   const [boxColors, setBoxColors] = useState(
     Array(25)
       .fill()
@@ -10,7 +9,6 @@ export default function ColorBox({ colors }) {
 
   // Used mdn web doc to help with Array function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-  // This function changes the color of the boxes to a random color when each small box is clicked.
   function changeColor(index) {
     const newColors = [...boxColors];
     newColors[index] = colors[Math.floor(Math.random() * colors.length)];
@@ -21,8 +19,8 @@ export default function ColorBox({ colors }) {
     <div className="ColorBoxesContainer">
       {boxColors.map((color, index) => (
         <div
-          key={index}
           className="ColorBox"
+          key={index}
           style={{ backgroundColor: color }}
           onClick={() => changeColor(index)}
         ></div>
